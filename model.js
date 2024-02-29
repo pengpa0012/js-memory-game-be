85da7259570145a9e5de1d91ddc342be83101227
Pengpa0012 <peng.ilikha@gmail.com>
2024-03-01 07:13:52 +0800

    added initial model

---
+++ model.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose')
+
+const scoreSchema = mongoose.Schema({
+  difficulty: { type: String, required: true },
+  score: { type: String, required: true },
+  date_created: [{type: Date, default: Date.now()}]
+});
+
+module.exports = mongoose.model('Scores', scoreSchema);
