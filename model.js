7ed950d4bdc9d7e6f1e8378203996d04fb3b29b5
Pengpa0012 <peng.ilikha@gmail.com>
2024-03-01 07:33:38 +0800

    update model, routes

--- model.js
+++ model.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const scoreSchema = mongoose.Schema({
   difficulty: { type: String, required: true },
   score: { type: String, required: true },
-  date_created: [{type: Date, default: Date.now()}]
+  date_created: {type: Date, default: Date.now()}
 });
 
 module.exports = mongoose.model('Scores', scoreSchema);
--- routes.js
+++ routes.js
@@ -1,8 +1,25 @@
 const express = require("express")
 require("dotenv").config()
 const router = express.Router()
+const API = require('./utils.js');
+const Scores = require("./model.js")
 
-router.get("/getScores", async (req, res) => {})
-router.post("/createScore", async (req, res) => {})
+router.get("/getScores", API.authenticateKey, async (req, res) => {
+  const result = await Scores.find()
+
+  return res.status(200).json({data: result})
+})
+
+router.post("/createScore", API.authenticateKey, async (req, res) => {
+  const {difficulty, score} = req.body
+  const newScore = new Scores({
+    difficulty,
+    score,
+    date_created: Date.now()
+  })
+  const result = await newScore.save()
+
+  return res.status(200).json({data: result, message: "Score Created!"})
+})
 
 module.exports = router
